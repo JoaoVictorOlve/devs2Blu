b314acddf59d0630474623229ceea2fb37469a21
JoaoVictorOlve <jvoliveira2005@gmail.com>
2023-01-18 21:54:06 -0300

    adicionando projeto completo django

--- 2023_01_11_venv/alunos/views.py
+++ 2023_01_11_venv/alunos/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Alunos
+
+
 
 def index(request):
-    return render(request, 'index.html')
 
-def aluno(request):
-    return render(request, 'aluno.html')
+    dados = Alunos.objects.all()
+
+    return render(request, "index.html", {'dados' : dados})
+    
+
+def aluno(request, aluno_id):
+    alunos = get_object_or_404(Alunos, pk=aluno_id)
+    aluno_a_exibir = {
+        'aluno' : alunos
+    }
+
+    return render(request, 'aluno.html', aluno_a_exibir)
+
+
+
 
 # Create your views here.
--- 2023_01_11_venv/raiz/settings.py
+++ 2023_01_11_venv/raiz/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'raiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER' : 'teste',
+        'PASSWORD' : '123456',
+        'HOST' : 'localhost',
+        'PORT' : '5435'
     }
 }
+
+
 
 
 # Password validation
