951ca0d78331c67361ea094cd146ec9b0c3c739e
JoaoVictorOlve <jvoliveira2005@gmail.com>
2023-02-03 21:48:34 -0300

    atualizando fastapi
    criei nova pasta do fastapi

--- 2023_01_26_fastapi/FST/main.py
+++ 2023_01_26_fastapi/FST/main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
-from fastapi import HTTPException, Query, Path, Header
-from fastapi import status
+from fastapi import FastAPI 
+
+from fastapi import HTTPException, status
 from models import Aluno, alunos
-from fastapi import Path
-from fastapi import Response, Depends
-from typing import Optional, Any
+from fastapi import Response
+from typing import Optional, Dict, List, Any
+from fastapi import Path, Query, Header, Depends
 from time import sleep
+
 
 app = FastAPI(
     title='MoreDevs2Blu',
-    version='0.6.7',
-    description='Desenvolvido pela turma da história'
+    version='0.6.9',
+    description='Desenvolvido Pela turma da historia'
 )
-
 def db():
     try:
         print('conexao com banco')
@@ -23,88 +23,74 @@ def db():
 
 @app.get('/')
 async def raiz():
-    return  {"mensagem":"Seja bem vindo ao more devs"}
+    return { "mensagem": "Seja bem vindo ao more devs" }
 
-# alunos = {
-#     1: {"Nome" : "Lirinha", "Idade" : "195", "E-mail" : "lirinha@gmail.com"},
-#     2: {"Nome" : "João", "Idade" : "15", "E-mail" : "joaosportclub@outlook.com"},
-#     3: {"Nome" : "Vander", "Idade" : "27", "E-mail" : "fiodovander@microsoft.com"}, 
-#     4: {"Nome" : "Thiago", "Idade" : "33", "E-mail" : "thiagogta4@hotmail.com"}
-# }
 
-@app.get('/alunos', description='lista de todos os alunos', summary='retorno substantivo', response_description='Lista alunos cadastrados')
-async def get_alunos():
+
+@app.get('/alunos', 
+description='lista de todos os alunos',
+summary='retorno substantivo',
+response_description="Lista de Alunos cadastrados"
+ )
+async def get_alunos(): 
     return alunos
-
-#Informa o último ID consultado
-@app.get("/alunos/{aluno_id}", description='Informa aluno pelo ID', summary='retorno individuo', response_description='Aluno cadastrado por ID')
-async def get_aluno(aluno_id: int = Path(default=None, title='ID Aluno', description='deve ser entre 1 ou 2', gt=0, lt=3), db :Any =Depends(db)):
+@app.get('/alunos/{aluno_id}')
+async def get_aluno(aluno_id: int = Path(default=None, title='ID Aluno', description='deve ser entre 1 ou 2', gt=0, lt=3), db :Any =Depends(db)): 
     try:
         aluno = alunos[aluno_id]
         return aluno
+
     except KeyError:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Aluno não encontrado"
+            status_code=status.HTTP_404_NOT_FOUND, detail='Aluno nao encontrado'
         )
 
-
-@app.post("/alunos", status_code=status.HTTP_201_CREATED, description='Cria novo aluno', summary='retorno individuo', response_description='Aluno criado')
-async def post_aluno(aluno:Aluno):
-    next_id : int = len(alunos) + 1
-    alunos[next_id] = aluno
-    del aluno.id
+@app.post('/alunos', status_code=status.HTTP_201_CREATED)
+async def post_aluno(aluno: Aluno):
+    next_id : int = len(alunos) +1
+    aluno.id = next_id
+    alunos.append(aluno)
     return aluno
 
-@app.get("/alunos/{aluno_name}", description='Informa aluno pelo nome', summary='retorno individuo', response_description='Aluno cadastrado por nome')
-async def get_aluno_by_name(aluno_name:str):
-    for aluno in alunos.values():
-        if aluno["Nome"] == aluno_name:
-            print(aluno["Nome"])
-            return aluno
 
-@app.put("/alunos/{aluno_id}", description='Atualiza aluno na lista de alunos', summary='retorno individuo', response_description='Aluno atualizado')
-async def put_aluno (aluno_id:int, aluno:Aluno):
+
+@app.put('/alunos/{aluno_id}')
+async def put_aluno(aluno_id: int, aluno: Aluno):
+    
     if aluno_id in alunos:
         alunos[aluno_id] = aluno
         del aluno.id
         return aluno
     else:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, detail="Aluno não encontrado"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Aluno nao encontrado com este id {aluno_id}')
 
-@app.get("/calc/", description='Efetua cálculos de acordo com a URL de requisição', summary='retorno substantivo', response_description='Resultado do cálculo')
-async def calc(valor_um:int = Query(default=None, gt=5), valor_dois:int = Query(default=None, gt=5), xdevs: str = Header(default=None), valor_tres: Optional[int] = None):
-    if valor_tres:
-        valor_final = valor_um + valor_dois + valor_tres
-    else:
-        valor_final = valor_um + valor_dois
-    print(f'devs: {xdevs}')
-    return valor_final
-    #raise HTTPException(
-    #    status_code = status.HTTP_200_OK, detail=f"Resultado de {valor_um}, {valor_dois} e {valor_tres} é {valor_final}"
-    #)
 
-@app.delete("/alunos/{aluno_id}", description='Remove aluno pelo ID', summary='retorno individuo', response_description='Aluno deletado')
-async def delete_aluno (aluno_id:int):
+@app.delete('/alunos/{aluno_id}')
+async def delete_aluno(aluno_id: int):
+    
     if aluno_id in alunos:
         del alunos[aluno_id]
-        raise HTTPException(
-            status_code = status.HTTP_200_OK, detail="Aluno deletado com sucesso"
-        )
-
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND, detail="Aluno não encontrado"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Aluno nao encontrado com este id {aluno_id}')
 
-if __name__ == "__main__":
+
+@app.get('/calculadora')
+async def calcular(a: int = Query(default=None, gt=5), b: int = Query(default=None, gt=5),  c:  Optional[int] = None, xdevs: str =Header(default=None)): 
+    soma = a + b 
+    if c:
+        soma = soma + c
+    print(f'devs: {xdevs}')
+    return soma
+
+
+if __name__ == '__main__':
     import uvicorn
 
     uvicorn.run(
         "main:app",
-        host = "127.0.0.1",
-        port = 8000,
+        host= "127.0.0.1",
+        port=8000,
         log_level = "info",
         reload = True
     )
--- 2023_01_26_fastapi/FST/models.py
+++ 2023_01_26_fastapi/FST/models.py
@@ -1,13 +1,19 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Aluno(BaseModel):
-    id : Optional[int] = None
-    nome : str
-    idade : int
-    email : str
+    id: Optional[int] = None
+    nome: str
+    idade: int
+    email: str
 
+    @validator('nome')
+    def validar_nome(cls, value: str):
+        abacate = value.split(' ')
+        if len(abacate) < 3:
+            raise ValueError('O Nome deve ter nome minimo 3 espacos')
+        return value
 alunos = [
-    Aluno(id=1, nome="João", idade=17, email="joao@gmail.com"),
-    Aluno(id=1, nome="Victor", idade=18, email="victor@gmail.com")
+    Aluno(id=1, nome="Andre vitor granemann", idade=25, email="andre@zuplae"),
+    Aluno(id=2, nome="Vitor belli pereira", idade=25, email="andre@zuplae")
 ]
